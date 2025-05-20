06c808b6d62bff69a9f9924a868ee32c7adc7146
felipecosta <fcosta.paltrinieri@gmail.com>
2025-05-20 19:17:29 -0300

    Upload src

---
+++ src/main/java/com/fiap/hotel_boa_pratica/HotelBoaPraticaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hotel_boa_pratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBoaPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBoaPraticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/hotel_boa_pratica/HotelBoaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hotel_boa_pratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBoaPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
